df481a6c650b76aeab902df9a02ac6dfe23bfd02
CorentinPolard <corentinpolard@gmail.com>
2025-09-03 10:23:03 +0200

    creation of header and burger menu

---
+++ assets/scripts/burgerMenu.js
@@ -0,0 +1,12 @@
+document.addEventListener("turbo:load", () => {
+    const burgerButton = document.querySelector(".burger-btn");
+    const navList = document.querySelector(".nav-list");
+    if (burgerButton) {
+        burgerButton.addEventListener("click", () => {
+            if (navList) {
+                navList.classList.toggle("active");
+                document.body.classList.toggle("no-scroll");
+            }
+        });
+    }
+})
