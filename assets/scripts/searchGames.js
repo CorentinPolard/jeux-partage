7260e82eab08a27d0b016c4780a748ff018e7451
CorentinPolard <corentinpolard@gmail.com>
2025-09-08 14:01:22 +0200

    Adding style to forms

--- assets/scripts/searchGames.js
+++ assets/scripts/searchGames.js
@@ -1,16 +1,18 @@
 import TomSelect from "tom-select";
 import "tom-select/dist/css/tom-select.default.min.css";
 
-document.addEventListener("turbo:load", () => {
+function initTomSelect() {
     const gamesList = document.querySelector(".games-list");
 
-    if (gamesList) {
-        new TomSelect(".games-list", {
+    // Si il y a le select et que le tomSelect n'a pas déjà été initialisé (turbo:load)
+    if (gamesList && !gamesList.tomselect) {
+        new TomSelect(gamesList, {
             plugins: ['remove_button'],
-            sortField: {
-                field: "text",
-                direction: "asc"
-            }
+            sortField: { field: "text", direction: "asc" }
         });
     }
-});
+};
+
+document.addEventListener("turbo:load", initTomSelect);
+// Pour le premier chargement si Turbo n'est pas encore actif
+initTomSelect();
