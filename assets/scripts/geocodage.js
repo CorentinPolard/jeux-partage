3ee116a91bdf5a5d651666638fe34e22f3903447
CorentinPolard <corentinpolard@gmail.com>
2025-09-10 09:24:50 +0200

    Added verification before requesting the geocoding API

--- assets/scripts/geocodage.js
+++ assets/scripts/geocodage.js
@@ -8,21 +8,26 @@ document.addEventListener("turbo:load", () => {
             const street = document.querySelector("#event_address_street");
             const city = document.querySelector("#event_address_city");
             const postcode = document.querySelector("#event_address_postcode");
+
             const longitude = document.querySelector("#event_address_longitude");
             const latitude = document.querySelector("#event_address_latitude");
 
             if (street && postcode && city) {
                 try {
-                    const response = await fetch(`https://data.geopf.fr/geocodage/search?q=${street.value}&postcode=${postcode.value}&city=${city.value}`);
+                    const url = `https://data.geopf.fr/geocodage/search?q=${encodeURIComponent(street.value)}&postcode=${encodeURIComponent(postcode.value)}&city=${encodeURIComponent(city.value)}`;
+                    const response = await fetch(url);
+                    if (!response.ok) {
+                        alert("Erreur liée à l'adresse lors de la soumission du formulaire.");
+                        return;
+                    }
                     const datas = await response.json();
                     if (!datas.features || datas.features.length === 0) {
                         alert("Aucune adresse trouvée !");
                         return;
-                    } else {
-                        const coordinates = [...datas.features[0].geometry.coordinates]; //longitude puis latitude
-                        longitude.value = coordinates[0];
-                        latitude.value = coordinates[1];
                     }
+                    const coordinates = [...datas.features[0].geometry.coordinates]; //longitude puis latitude
+                    longitude.value = coordinates[0];
+                    latitude.value = coordinates[1];
                 } catch (error) {
                     console.error(error);
                 }
