d0b9bb480259a565fb63229c64d718bc31548b8f
CorentinPolard <corentinpolard@gmail.com>
2025-09-11 09:11:45 +0200

    category management for admin

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -2,18 +2,69 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/categories')]
 final class AdminCategoryController extends AbstractController
 {
     #[Route('', name: 'app_admin_categories')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findAll();
+
         return $this->render('admin_category/index.html.twig', [
-            'controller_name' => 'AdminCategoryController',
+            'categories' => $categories,
         ]);
+    }
+
+    #[Route('/create', name: 'app_admin_create_category')]
+    public function createCategory(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_admin_categories');
+        }
+
+        return $this->render('admin_category/create-category.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'app_admin_edit_category', requirements: ['id' => '\d+'])]
+    public function editCategory(Category $category, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_admin_categories');
+        }
+
+        return $this->render('admin_category/create-category.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'app_admin_delete_category', requirements: ['id' => '\d+'])]
+    public function deleteCategory(Category $category, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($category);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_admin_categories');
     }
 }
