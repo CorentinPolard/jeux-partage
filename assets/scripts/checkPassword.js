d92b0f5567ee5c2a75048efe10fa9c76b8707ed9
CorentinPolard <corentinpolard@gmail.com>
2025-09-17 10:20:33 +0200

    Fix: Compare the password at the form submission and no longer to the empty input

--- assets/scripts/checkPassword.js
+++ assets/scripts/checkPassword.js
@@ -2,7 +2,6 @@ const form = document.querySelector("#registrationForm")
 const passwordInput = document.querySelector("#registration_form_plainPassword_first");
 const passwordHelp = document.querySelector(".password-help");
 const passwordValidators = document.querySelectorAll(".password-validator");
-const password = passwordInput.value;
 const regex = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,}$/;
 
 passwordInput.addEventListener("input", () => {
@@ -19,7 +18,7 @@ passwordInput.addEventListener("input", () => {
 })
 
 form.addEventListener("submit", (event) => {
-    if (!regex.test(password)) {
+    if (!regex.test(passwordInput.value)) {
         event.preventDefault();
         alert("Votre mot de passe ne respecte pas les critères requis !");
         passwordHelp.classList.remove("hidden");
