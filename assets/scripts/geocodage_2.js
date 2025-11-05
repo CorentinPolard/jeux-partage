09b3b1a96729843717fc659561e465b2dbf025a0
CorentinPolard <corentinpolard@gmail.com>
2025-11-05 12:22:52 +0100

    Fonctional events form with autocompletion for addresses

--- assets/scripts/geocodage_2.js
+++ assets/scripts/geocodage_2.js
@@ -1,7 +1,12 @@
 const form = document.querySelector("#eventForm");
 if (form) {
     const streetField = document.querySelector("#event_address_street");
-    // const url = `https://data.geopf.fr/geocodage/completion/?text=${encodeURIComponent(streetField.value)}&postcode=${encodeURIComponent(postcode.value)}&city=${encodeURIComponent(city.value)}`;
+    const cityField = document.querySelector("#event_address_city");
+    const postcodeField = document.querySelector("#event_address_postcode");
+
+    const longitudeField = document.querySelector("#event_address_longitude");
+    const latitudeField = document.querySelector("#event_address_latitude");
+
     const url = "https://data.geopf.fr/geocodage/completion/?text=";
     const addressesSuggestions = document.querySelector("#addresses-suggestions");
 
@@ -12,11 +17,11 @@ if (form) {
             timeoutId = setTimeout(async () => {
                 await findAddresses(streetFieldValue);
             }, 300);
-        } else {
-            console.log("mauvaise longueur")
         }
     })
 
+    // Search addresses corresponding
+    // And shox the user the suggestion
     async function findAddresses(street) {
         const url = "https://data.geopf.fr/geocodage/completion/?text=";
         try {
@@ -26,14 +31,69 @@ if (form) {
                 return;
             }
             const datas = await response.json();
-            console.log(datas);
             if (datas.results && datas.results.length > 0) {
                 addressesSuggestions.innerHTML = "";
-                datas.results.map(result => addressesSuggestions.innerHTML += `<li>${result.fulltext}</li>`);
+                datas.results.map(result => addressesSuggestions.innerHTML += `<li id="address-suggestion" class="address-suggestion" data-longitude="${result.x}" data-latitude="${result.y}">${result.fulltext}</li>`);
+                initAdrressValidation();
+                addressesSuggestions.classList.remove("hidden");
             }
         } catch (e) {
             console.error(e);
         }
     }
+
+    // For each addresses :
+    // I add an click event listener
+    // which fill the address fields and hide the suggestion list
+    function initAdrressValidation() {
+        const addresses = document.querySelectorAll("#address-suggestion");
+        addresses.forEach((address) => {
+            address.addEventListener("click", (e) => {
+                const fullAddress = e.target.innerText.split(",");
+
+                streetField.value = fullAddress[0];
+                postcodeField.value = fullAddress[1].substring(0, 6);
+                cityField.value = fullAddress[1].substring(6);
+                longitudeField.value = e.target.dataset.longitude;
+                latitudeField.value = e.target.dataset.latitude;
+
+                addressesSuggestions.classList.add("hidden");
+            })
+        })
+    }
+
+    // On submit : 
+    // Check if there latitude and longitudes fields are filled
+    // Complete in needs
+    form.addEventListener("submit", async function (e) {
+        e.preventDefault();
+
+        if (streetField.value.trim() == "" || postcodeField.value.trim() == "" || cityField.value.trim() == "") {
+            alert("Certains champs d'adresse sont manquants !");
+            return;
+        } else if (latitudeField.value.trim() == "") {
+            console.log("try")
+            try {
+                const url = `https://data.geopf.fr/geocodage/search?index=address,poi&q=${encodeURIComponent(streetField.value)}&postcode=${encodeURIComponent(postcodeField.value)}&city=${encodeURIComponent(cityField.value)}`;
+                const response = await fetch(url);
+                if (!response.ok) {
+                    alert("Erreur liée à l'adresse lors de la soumission du formulaire.");
+                    return;
+                }
+                const datas = await response.json();
+                if (!datas.features || datas.features.length === 0) {
+                    alert("Aucune adresse trouvée !");
+                    return;
+                }
+                const coordinates = [...datas.features[0].geometry.coordinates]; //longitude puis latitude
+                longitudeField.value = coordinates[0];
+                latitudeField.value = coordinates[1];
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        form.submit();
+    })
 
 } 
