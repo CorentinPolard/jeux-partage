9e7acb3318d9f0f7d59dbfe43fe6bb968888071c
CorentinPolard <corentinpolard@gmail.com>
2025-11-06 09:37:00 +0100

    Fix: Giving unique ids to li elements and changing variables names url to search_url and completion_url

--- assets/scripts/geocodage_2.js
+++ assets/scripts/geocodage_2.js
@@ -7,7 +7,7 @@ if (form) {
     const longitudeField = document.querySelector("#event_address_longitude");
     const latitudeField = document.querySelector("#event_address_latitude");
 
-    const url = "https://data.geopf.fr/geocodage/completion/?text=";
+    const completion_url = "https://data.geopf.fr/geocodage/completion/?text=";
     const addressesSuggestions = document.querySelector("#addresses-suggestions");
 
     let timeoutId;
@@ -29,7 +29,7 @@ if (form) {
     async function findAddresses(street) {
         const url = "https://data.geopf.fr/geocodage/completion/?text=";
         try {
-            const response = await fetch(url + encodeURIComponent(street));
+            const response = await fetch(completion_url + encodeURIComponent(street));
             if (!response.ok) {
                 console.log("response not ok");
                 return;
@@ -37,7 +37,7 @@ if (form) {
             const datas = await response.json();
             if (datas.results && datas.results.length > 0) {
                 addressesSuggestions.innerHTML = "";
-                datas.results.map(result => addressesSuggestions.innerHTML += `<li id="address-suggestion" class="address-suggestion" data-longitude="${result.x}" data-latitude="${result.y}">${result.fulltext}</li>`);
+                datas.results.map(result => addressesSuggestions.innerHTML += `<li class="address-suggestion" data-name="address-suggestion" data-longitude="${result.x}" data-latitude="${result.y}">${result.fulltext}</li>`);
                 initAdrressValidation();
                 addressesSuggestions.classList.remove("hidden");
             }
@@ -50,7 +50,7 @@ if (form) {
     // I add an click event listener
     // which fill the address fields and hide the suggestion list
     function initAdrressValidation() {
-        const addresses = document.querySelectorAll("#address-suggestion");
+        const addresses = document.querySelectorAll("li[data-name=address-suggestion]");
         addresses.forEach((address) => {
             address.addEventListener("click", (e) => {
                 const fullAddress = e.target.innerText.split(",");
@@ -78,8 +78,8 @@ if (form) {
         } else if (latitudeField.value.trim() == "") {
             console.log("try")
             try {
-                const url = `https://data.geopf.fr/geocodage/search?index=address,poi&q=${encodeURIComponent(streetField.value)}&postcode=${encodeURIComponent(postcodeField.value)}&city=${encodeURIComponent(cityField.value)}`;
-                const response = await fetch(url);
+                const search_url = `https://data.geopf.fr/geocodage/search?index=address,poi&q=${encodeURIComponent(streetField.value)}&postcode=${encodeURIComponent(postcodeField.value)}&city=${encodeURIComponent(cityField.value)}`;
+                const response = await fetch(search_url);
                 if (!response.ok) {
                     alert("Erreur liée à l'adresse lors de la soumission du formulaire.");
                     return;
