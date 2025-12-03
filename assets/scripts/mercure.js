7205a9a5ed8455b46fa8183eb569d96cba51bb9b
CorentinPolard <corentinpolard@gmail.com>
2025-12-03 17:11:28 +0100

    trying to fix mercure issue on prod

--- assets/scripts/mercure.js
+++ assets/scripts/mercure.js
@@ -11,7 +11,10 @@ const currentUserId = messagesContainer.dataset.userId;
 // Connection to mercure hub
 const topic = messagesContainer.dataset.topic;
 const mercureToken = messagesContainer.dataset.mercureToken;
-const url = new URL('https://jeuxpartage.fr/.well-known/mercure');
+// En mode prod (l'une des deux suivantes) :
+// const url = new URL('https://jeuxpartage.fr/.well-known/mercure');
+const url = new URL('/.well-known/mercure', window.location.origin);
+// En mode dev :
 // const url = new URL('http://127.0.0.1:3000/.well-known/mercure');
 url.searchParams.append('topic', topic);
 url.searchParams.append('jwt', mercureToken);
